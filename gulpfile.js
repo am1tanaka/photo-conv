6ee4caf22a03ebcd8b44b4e2809bb528ebbafbf1
YuTanaka <YRK00337@nifty.com>
2015-07-14 00:12:55 +0900

    開発環境の登録

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+var watch = require('gulp-watch');
+
+var destpath = "/Library/WebServer/Documents/photo-conv/";
+
+gulp.task('web', function() {
+  gulp.src("web/*.*")
+    .pipe(gulp.dest(destpath));
+  gulp.src("node_modules/jquery/dist/jquery.min.js")
+    .pipe(gulp.dest(destpath+"plugins/"));
+  gulp.src("node_modules/bootstrap/dist/**/*.*")
+    .pipe(gulp.dest(destpath+"plugins/"));
+});
+
+gulp.task('reload', function() {
+  reload();
+});
+
+// 変更とライブリロードの監視を起動
+gulp.task('watch', ['web'], function() {
+  browserSync.init({
+    proxy: 'localhost/photo-conv/'
+  });
+
+  gulp.watch([
+    'web/*.html',
+    'web/*.php'],
+    ['web','reload']
+  );
+});
+
+gulp.task('default', ['watch']);
---
+++ web/photo-conv.php
@@ -0,0 +1,18 @@
+<?php
+foreach($_FILES['input_file']['error'] as $key => $error) {
+    if ($error == 0) {
+      $tmp_name = $_FILES['input_file']['tmp_name'][$key];
+      $name = $_FILES['input_file']['name'][$key];
+      $type = $_FILES['input_file']['type'][$key];
+      $size = $_FILES['input_file']['size'][$key];
+      echo "FILES[tmp_name][$key]=".$tmp_name."<br/>";
+      echo "FILES[name][$key]=".$name."<br/>";
+      echo "FILES[type][$key]=".$type."<br/>";
+      echo "FILES[size][$key]=".$size."<br/>";
+    }
+    else {
+      echo "error:".$error."<br/>";
+    }
+}
+
+ ?>
